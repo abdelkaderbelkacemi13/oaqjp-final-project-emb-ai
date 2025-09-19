279fea3f4209cf534a795a8f5103acdbd2a8b782
Abdelkader Belkacemi <belkacemi.abdelkader54@gmail.com>
2025-09-19 15:51:32 +0100

    new task

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,4 +8,28 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers=headers, data=json.dumps(input_json))
     
-    return response.text
+    emotion_results = json.loads(response.text)
+
+    user_emotion = emotion_results['emotion_predictions'][0]['emotion']
+    anger = user_emotion['anger']
+    disgust = user_emotion['disgust']
+    fear = user_emotion['fear']
+    joy = user_emotion['joy']
+    sadness = user_emotion['sadness']
+
+    scores = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness
+    }
+    final_result = max(scores, key=scores.get)
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': final_result
+    }
