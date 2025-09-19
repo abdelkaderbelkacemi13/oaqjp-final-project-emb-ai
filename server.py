0fab855d91c625f2e66865244291db603220b136
Abdelkader Belkacemi <belkacemi.abdelkader54@gmail.com>
2025-09-19 21:03:19 +0100

    update

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection App")
 
 @app.route("/")
 def home():
-    return render_template("template/index.html")
+    return render_template("index.html")
 
 
-@app.route("/detect_emotion", methods=["Get","POST"])
+@app.route("/emotionDetector")
 def emotiondetector():
     text_to_analyze = request.args.get('textToAnalyze')
     emotions_result = emotion_detector(text_to_analyze)
@@ -21,8 +21,11 @@ def emotiondetector():
     sadness = emotions_result['sadness']
     dominant_emotion = emotions_result['dominant_emotion']
     
-    return f"For the given statement, the system response is " \
-       f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, " \
-       f"'joy': {joy}, and 'sadness': {sadness}.\nThe dominant emotion is {dominant_emotion}."
+    formatted_output = (
+    f"For the given statement, the system response is 'anger': {anger}, "
+    f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} "
+    f"and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}.")
+    
+    return formatted_output
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
